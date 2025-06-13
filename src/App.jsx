f7d143e56c3b5586577f979cd670e460acd52742
subhrajyoti11 <goudabalunkeswar@gmail.com>
2025-06-13 20:13:12 +0530

    react projects

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-         <Navbar />
+         <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/doctors' element={<Doctors/>}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+<div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#ADADAD]'>
+      <img onClick={() => navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <ul className='md:flex items-start gap-5 font-medium hidden'>
+        <NavLink to='/' >
+          <li className='py-1'>HOME</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/doctors' >
+          <li className='py-1'>ALL DOCTORS</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/about' >
+          <li className='py-1'>ABOUT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/contact' >
+          <li className='py-1'>CONTACT</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
+        </NavLink>
+      </ul>
 
-  
-    <NavLink>
-        <li>Home</li>
-    </NavLink>
-        <NavLink>
-        <li>All Doctors</li>
-    </NavLink>
-        <NavLink>
-        <li>About</li>
-    </NavLink>
-        <NavLink>
-        <li>Contact</li>
-    </NavLink>
-  
-  <div>
-    <button>Create account</button>
-  </div>
     </div>
   )
 }
